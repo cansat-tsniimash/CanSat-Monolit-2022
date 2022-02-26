f9a6e01daa4f2e8a593ba5eff7104a2302b32207
Jly4e3apHbiu <pasha0000407@mail.ru>
2022-02-26 18:27:38 +0300

    заигнорил файлы папки дебуг

--- Research/gps/Debug/Application/gps.d
+++
@@ -1,3 +0,0 @@
-Application/gps.o: ../Application/gps.c ../Application/gps.h
-
-../Application/gps.h:
--- Research/gps/Debug/Application/nmea_gps.d
+++
@@ -1,6 +0,0 @@
-Application/nmea_gps.o: ../Application/nmea_gps.c \
- ../Application/nmea_gps.h ../Application/minmea/minmea.h
-
-../Application/nmea_gps.h:
-
-../Application/minmea/minmea.h:
